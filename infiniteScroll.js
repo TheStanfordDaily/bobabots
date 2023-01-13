6ad3cc538e18728f4f2d3dce64f15143901acfc6
Matthew Turk <mjturk15@gmail.com>
2023-01-12 22:40:43 -0800

    Minor refactoring

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -23,7 +23,6 @@ class InfiniteScroll {
         console.log("Initializing infinite scroll...")
         this.resetFilter()
         this.simulateKeyboardEvent(FILTER_ID, department)
-
         this.department = department
         this.currentScrollHeight = 0
         this.numberOfScrolls = 0
@@ -39,23 +38,20 @@ class InfiniteScroll {
         this.currentScrollHeight = document.body.scrollHeight
         window.scrollTo(0, this.currentScrollHeight)
         await randomSleep()
+        
         if (this.currentScrollHeight === document.body.scrollHeight) {
             this.numberOfTrials++
             const attemptsRemaining = MAXIMUM_NTRIALS - this.numberOfTrials
             let updateMessage = "Bottom of scroll window detected. Will check for additional content" + attemptsRemaining.toString() + " more time"
             updateMessage += attemptsRemaining === 1 ? "..." : "s..."
-            console.log(`Bottom of scroll window detected. Will check for additional content ${MAXIMUM_NTRIALS - this.numberOfTrials} more time(s)...`)
+            console.log(updateMessage)
         } else {
             this.numberOfTrials = 0
             this.numberOfScrolls++
             console.log(`Scroll ${this.numberOfScrolls} was successful!`)
         }
 
-        if (this.entriesRemain()) {
-            await this.exhaust()
-        } else {
-            this.summarize()
-        }
+        this.entriesRemain() ? await this.exhaust() : this.summarize()
     }
 
     summarize() {
@@ -66,15 +62,13 @@ class InfiniteScroll {
 
     entriesRemain() {
         const elements = Array.prototype.slice.call(document.getElementsByClassName(PERSON_DATA_CLASS))
-        // return whether every element has text containing this.department
         return elements.every(element => element.innerText.includes(this.department))
     }
 
     simulateKeyboardEvent(id, text) {
         const input = document.getElementById(id)
-        const event = new Event("input", { bubbles: true })
         input.value += text
-        input.dispatchEvent(event)
+        input.dispatchEvent(new Event("input", { bubbles: true }))
     }
 
     resetFilter() {
@@ -83,9 +77,9 @@ class InfiniteScroll {
     }
 }
 
-const depts = ["English", "Music", "History"]
+const departments = ["English", "Music", "History"]
 
-for (let i = 0; i < depts.length; i++) {
-    const infiniteScroll = new InfiniteScroll(depts[i])
+for (let department of departments) {
+    const infiniteScroll = new InfiniteScroll(department)
     await infiniteScroll.exhaust()
 }
