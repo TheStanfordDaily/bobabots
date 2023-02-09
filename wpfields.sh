d95fbd8d72842dd0c35ed9728569a18dbf9bb3ae
Matthew Turk <mjturk15@gmail.com>
2023-02-08 22:29:11 -0800

    Minor refactoring

--- wpfields.sh
+++ wpfields.sh
@@ -3,15 +3,15 @@ offset=0
 
 # Initialize the batch size
 batch_size=999
-re='^[0-9]+$'
+digit="^[0-9]+$"
+batch="^[0-9]+"
 
-# Begin while true loop
 while true; do
     # Get the IDs of the posts in the current batch
     posts=$(vip @stanforddaily.preprod -- wp post list --field=ID --offset=$offset --posts_per_page=$batch_size)
   
     # If there are no posts in the current batch, exit the loop
-    if [ -z "$posts" ]; then
+    if ! [[ $posts =~ $batch ]]; then
         break
     fi
 
@@ -20,11 +20,11 @@ while true; do
     # Loop through each post in the current batch
     for post in $posts; do
         # Get the value of the `primary_term_category` field for the current post
-        echo "Processing post $post"
+        echo "Processing post ID $post"
         primary_term_category=$(vip @stanforddaily.preprod -- wp post meta get $post _primary_term_category)
         
         # Check against re
-        if ! [[ $primary_term_category =~ $re ]] ; then
+        if ! [[ $primary_term_category =~ $digit ]]; then
             # If the `primary_term_category` field is empty, skip to the next post
             continue
         fi
