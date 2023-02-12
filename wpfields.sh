96ec4d773144cce7fa16e02294a9a1cd3474e739
Matthew Turk <mjturk15@gmail.com>
2023-02-12 11:26:42 -0800

    Will try a more economical approach

--- wpfields.sh
+++ wpfields.sh
@@ -1,12 +1,14 @@
 # Read in post IDs from post_ids.txt
 while read post_id; do
     echo "Post ID: $post_id"
-    # vip @stanforddaily.develop -- wp post meta get $post_id _primary_term_category >> with_cats.txt &
+    "$post_id,$(vip @stanforddaily.develop -- wp post meta get "$post_id" _primary_term_category)" >> id_cats.txt &
     # Retrieve the _primary_term_category value for the post ID
-    primary_term_category=$(vip @stanforddaily.develop -- wp post meta get $post_id _primary_term_category) &
-    echo "Post ID: $post_id | Primary Term Category: $primary_term_category"
+    # primary_term_category=$(vip @stanforddaily.develop -- wp post meta get $post_id _primary_term_category) &
+    # echo "Post ID: $post_id | Primary Term Category: $primary_term_category"
 
-    # Write the post ID and _primary_term_category value to with_cats.txt
-    # Remember to remove lines that have no _primary_term_category value
-    echo "vip @stanforddaily.develop -- wp post meta update $post_id rank_math_primary_category $primary_term_category" >> with_cats.sh &
+    # # Write the post ID and _primary_term_category value to with_cats.txt
+    # # Remember to remove lines that have no _primary_term_category value
+    # echo "vip @stanforddaily.develop -- wp post meta update $post_id rank_math_primary_category $primary_term_category" >> with_cats.sh &
 done < post_ids.txt
+
+wait
