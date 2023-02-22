383c1daa93277ba0b6b9d0a7e504eecfc2bb9b11
Matthew Turk <mjturk15@gmail.com>
2023-02-21 19:39:39 -0800

    Minor refactoring

--- ingest_ids.py
+++ ingest_ids.py
@@ -1,17 +1,19 @@
 import os
 from tqdm import tqdm
 
+
 def populate_rank_math_categories(post_ids: list[str]):
     for post_id in tqdm(post_ids):
         p1 = os.popen(f"vip @stanforddaily.develop -- wp post meta get {post_id} _primary_term_category")
-        output = p1.read().strip()
+        category = p1.read().strip()
         p1.close()
 
-        if len(output) > 0:
-            p2 = os.popen(f"vip @stanforddaily.develop -- wp post meta update {post_id} rank_math_primary_category {output}")
+        if len(category) > 0:
+            p2 = os.popen(f"vip @stanforddaily.develop -- wp post meta update {post_id} rank_math_primary_category {category}")
             p2.read()
             p2.close()
-            print(f"Successfully updated {post_id} with {output}")
+            print(f"Successfully updated {post_id} with {category}")
+
 
 if __name__ == "__main__":
     with open("post_ids.txt") as f:
