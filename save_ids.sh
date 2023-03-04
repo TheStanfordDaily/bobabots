e0fca057c9c2a841ae778d9d54587b3bc1a16fc8
Matthew Turk <mjturk15@gmail.com>
2023-03-03 22:14:26 -0800

    Write to `categories-n.sh`

--- save_ids.sh
+++ save_ids.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Initialize the batch starting point.
 offset=0
 
@@ -7,19 +8,8 @@ batch_size=999
 # Define regular expression to validate batch.
 batch="^[0-9]+"
 
-while true; do
-    # Get the IDs of the posts in the current batch.
-    # This call only works if you have the VIP CLI installed and are an authenticated user.
-    posts=$(vip @stanforddaily.develop -- wp post list --field=ID --offset=$offset --posts_per_page=$batch_size)
-    
-    # If there are no posts in the current batch, exit the loop.
-    if ! [[ $posts =~ $batch ]]; then
-        break
-    fi
-
-    # Write post IDs to post_ids.txt
-    echo "$posts" >> post_ids.txt
-
-    # Move to the next batch.
-    offset=$((offset + batch_size))
-done
+# read in lines from post_ids_n.txt
+while read -r line; do
+    rank_math=$(vip @stanforddaily.preprod -- wp post meta get "$line" _primary_term_category)
+    echo "echo y | vip @stanforddaily.production -- wp post meta update $line rank_math_primary_category $rank_math" >> categories-n.sh
+done < post_ids_n.txt
