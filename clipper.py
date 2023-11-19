6e8968a901497f7d576f1440dcb037f9552a3e3a
Matthew Turk <mjturk15@gmail.com>
2023-11-19 12:48:56 -0800

    Initial implementation of Daily link to PDF

---
+++ clipper.py
@@ -0,0 +1,181 @@
+import os
+import re
+import requests
+import sys
+from pylatex import Document, Section, Subsection, Package, Command, Figure, NewLine
+from pylatex.utils import italic, NoEscape
+from bs4 import BeautifulSoup, PageElement, Tag
+from utils import itemize, iso_to_ap
+
+API_ENDPOINT = "https://stanforddaily.com/wp-json/wp/v2/posts"
+MINION_OPTIONS = {
+    "Path": NoEscape("./MinionFontFiles/"),
+    "Extension": ".ttf",
+    "UprightFont": "*Regular",
+    "BoldFont": "*Bold",
+    "ItalicFont": "*It",
+    "BoldItalicFont": "*BoldIt"
+}
+
+
+class Author:
+    def __init__(self, name: str, url: str = ""):
+        self.name = name
+        self.url = url
+
+    def __str__(self):
+        return self.name
+
+
+class Clipper:
+    def __init__(self, article: str):
+        # Case 1: Input is a URL with slug.
+        try:
+            self.slug = re.search(r"/(\d{4}/\d{2}/\d{2})/([\w-]*)/?", article).group(2)
+        except AttributeError:
+            # Case 2: Input is a URL with ID.
+            try:
+                self.slug = re.search(r"\?p=(\d+)", article).group(1)
+            except AttributeError:
+                # Case 3: Input is a slug.
+                self.slug = article
+        self.post_data = self.fetch_wordpress_post()
+        self.doc = Document(font_size="large")
+        self.soup = BeautifulSoup(self.post_data[0]["content"]["rendered"], "html.parser")
+        self.aux_files = []
+
+    def __del__(self):
+        for filename in self.aux_files:
+            os.remove(filename)
+
+    def fetch_wordpress_post(self):
+        params = {"slug": self.slug, "_embed": "true"}
+        response = requests.get(API_ENDPOINT, params=params)
+        if response.status_code == 200:
+            return response.json()
+
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+    def parse_html(self, node: PageElement | Tag) -> str:
+        if node.name is None:
+            return node.string
+
+        latex_content = NoEscape("")
+
+        # Recursively parse child nodes in the DOM tree.
+        for child in node.children:
+            latex_content += NoEscape(self.parse_html(child))
+
+        if node.name == "p":
+            # Wrap paragraph content in LaTeX command, just in case.
+            return Command("par", latex_content).dumps() + r"\\"
+        elif node.name == "strong":
+            # Convert <strong> to \textbf{}.
+            return Command("textbf", latex_content).dumps()
+        elif node.name == "a":
+            # Convert <a href="..."> to \href{...}{...}.
+            href = node.get("href", "")
+            return Command("href", arguments=[NoEscape(href), latex_content]).dumps()
+        elif node.name == "img":
+            caption = node.find_next("figcaption")
+            caption_text = caption.string if caption is not None else ""
+            try:
+                img_url = node.get("src")
+            except KeyError:
+                img_url = None
+            fig = Figure(position="ht!")
+            if img_url is None:
+                return fig.dumps()
+            response = requests.get(img_url)
+            content = response.content
+
+            # Save image to auxiliary file in local directory.
+            aux_slug = [x for x in img_url.split("/") if len(x) > 0][-1]
+            aux_path = f"{aux_slug}.jpg"
+            with open(aux_path, "wb") as file:
+                file.write(content)
+            self.aux_files.append(aux_path)
+
+            fig.add_image(aux_path, width="140mm")
+            fig.add_caption(italic(caption_text))
+
+            return fig.dumps()
+        elif node.name == "figcaption":
+            return ""  # Ignore captions, since they are handled by <img> tags.
+        # TODO: Add support for other tags.
+
+        return latex_content
+
+    def create_latex_document(self):
+        self.doc.packages.append(Package("geometry"))
+        self.doc.preamble.append(Package("parskip"))
+        self.doc.preamble.append(Package("titling"))
+        self.doc.preamble.append(Command("pretitle", NoEscape(r"\vspace{-4em}\begin{center}\includegraphics[width=0.5\textwidth]{logo.pdf}\end{center}\begin{flushleft}\Large\bfseries")))
+        self.doc.preamble.append(Command("posttitle", NoEscape(r"\par\end{flushleft}")))
+        self.doc.preamble.append(Command("preauthor", NoEscape(r"\begin{flushleft}\Large By ")))
+        self.doc.preamble.append(Command("postauthor", NoEscape(r"\end{flushleft}")))
+        self.doc.preamble.append(Command("predate", NoEscape(r"\begin{flushleft}")))
+        self.doc.preamble.append(Command("postdate", NoEscape(r"\end{flushleft}")))
+        self.doc.preamble.append(Command("usepackage", "graphicx"))
+        self.doc.preamble.append(Command("pagenumbering", "gobble"))
+        # self.doc.preamble.append(Package("fancyhdr"))
+        # self.doc.preamble.append(Command('pagestyle', 'fancy'))
+        # self.doc.preamble.append(Command('fancyhf', ''))  # Clear default header and footer
+        # self.doc.preamble.append(Command('renewcommand', '\\headrulewidth', extra_arguments='0pt'))  # Remove header rule
+        # self.doc.preamble.append(Command('fancyhead', 'C', extra_arguments=NoEscape(r'\includegraphics[width=0.5in]{logo}')))
+        # print(NoEscape(r'\includegraphics[width=0.5in]{logo.png}'))
+        # self.doc.preamble.append(Command('renewcommand', NoEscape('\\headrulewidth'), extra_arguments='0pt'))
+
+        self.doc.preamble.append(Command("usepackage", "hyperref", options=NoEscape(r"colorlinks=true,urlcolor=blue")))
+        self.doc.preamble.append(Command("usepackage", "fontspec"))
+        self.doc.preamble.append(Command("usepackage", "caption"))
+        self.doc.preamble.append(Command("captionsetup", "labelformat=empty,width=140mm"))
+        self.doc.preamble.append(Command("geometry", arguments=NoEscape(r"a4paper,total={170mm,257mm},left=20mm,top=20mm,")))
+        self.doc.preamble.append(Command("pagestyle", "empty"))
+        doc_title = Command("title", Command("huge", Command("textbf", NoEscape(
+            BeautifulSoup(self.post_data[0]["title"]["rendered"], "html.parser").get_text()))))
+        self.doc.preamble.append(doc_title)
+        self.doc.preamble.append(Command("setmainfont", "MinionPro", options=MINION_OPTIONS))
+
+        try:
+            authors = [Author(name=n) for n in self.post_data[0]["parsely"]["meta"]["creator"]]
+            for index, author in enumerate(authors):
+                author.url = self.post_data[0]["_embedded"]["author"][index]["link"]
+        except KeyError:
+            # TODO: Use post["author"] or post["coauthors"] to fill in author names.
+            authors = []
+        author_names = [NoEscape(Command("href", arguments=[NoEscape(a.url), a.name]).dumps()) for a
+                        in authors]
+        self.doc.preamble.append(Command("author", itemize(author_names)))
+        self.doc.preamble.append(Command("date", iso_to_ap(self.post_data[0]["date"])))
+        self.doc.append(NoEscape(r"\maketitle"))
+        soup = BeautifulSoup(self.post_data[0]["content"]["rendered"], "html.parser")
+        parsed = self.parse_html(soup)
+        # A bit of a hack, but it will do for now...
+        parsed = parsed.replace("\\textbackslash{}", "\\").replace("\\{", "{").replace("\\}", "}")
+        self.doc.append(NoEscape(parsed))
+        # print(NoEscape(parsed))
+        # might implement this later for h2 tags and whatnot
+        # with doc.create(Section('WordPress Post')):
+
+        return self.doc
+
+
+def main():
+    try:
+        article_url = sys.argv[1]
+    except IndexError as error:
+        raise ValueError(f"Please provide article URL, ID or slug as the first positional argument.\n{error}")
+    clipper = Clipper(article_url)
+    doc = clipper.create_latex_document()
+    # Save the document using XeLaTeX engine (otherwise, custom fonts would not work).
+
+    doc.generate_pdf(clipper.slug, compiler="xelatex")
+    # Move to out folder
+    if not os.path.exists("out"):
+        os.mkdir("out")
+    os.rename(f"{clipper.slug}.pdf", os.path.join("out", f"{clipper.slug}.pdf"))
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import re
+from datetime import datetime
 
 
 def swap_files(file1, file2):
@@ -44,3 +45,46 @@ def load_env() -> tuple:
         raise Exception("No CLIENT_KEY or CLIENT_SECRET in .env")
 
     return pairs["CLIENT_KEY"], pairs["CLIENT_SECRET"]
+
+
+def itemize(items):
+    len_items = len(items)
+    if len_items == 0:
+        return ""
+    elif len_items == 1:
+        return items[0]
+    return ", ".join(items[:-1]) + " and " + items[-1]
+
+
+def format_date(instance: datetime, verbose=False) -> str:
+    """
+    Formats a datetime instance into a string following AP style.
+
+    Args:
+    instance (datetime): The datetime instance to format.
+    verbose (bool): Whether to include the time in the formatted string.
+
+    Returns:
+    str: A string representation of the date.
+    """
+    months = ["Jan.", "Feb.", "March", "April", "May", "June",
+              "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."]
+
+    formatted_month = months[instance.month - 1]
+    formatted_day = instance.day
+    formatted_year = instance.year
+
+    if verbose:
+        formatted_hours = instance.hour % 12
+        formatted_hours = formatted_hours if formatted_hours else 12
+        formatted_minutes = f"{instance.minute:02d}"
+        formatted_meridian = "a.m." if instance.hour < 12 else "p.m."
+
+        return f"{formatted_month} {formatted_day}, {formatted_year}, {formatted_hours}:{formatted_minutes} {formatted_meridian}"
+    else:
+        return f"{formatted_month} {formatted_day}, {formatted_year}"
+
+
+def iso_to_ap(iso: str) -> str:
+    iso_date = datetime.fromisoformat(iso)
+    return format_date(iso_date, verbose=True)
